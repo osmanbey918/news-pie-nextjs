80482004edd6b131894b66a2c2c74a90462cab9f
Usman Khan <usman918khan@gmail.com>
2025-03-11 21:23:58 +0500

    successfully-implemented-newsapi

--- src/app/page.js
+++ src/app/page.js
@@ -1,22 +1,65 @@
-import Header from '../components/header/Header'
-import Footer from '../components/footer/Footer'
-import Head from 'next/head';
-import NewsList from '../components/news/NewsList';
+"use client";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function Home() {
+  const [articles, setArticles] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchNews = async () => {
+      try {
+        const response = await axios.get("https://newsdata.io/api/1/latest?apikey=pub_7406001aa4474f3513c9007f270b615f01523&q=pizza", {
+          // params: {
+          //   q: "Apple",
+          //   from: "2025-03-11",
+          //   sortBy: "popularity",
+          //   apiKey: process.env.NEXT_PUBLIC_NEWS_API_KEY, // Corrected usage
+          // },
+        });
+        console.log(response.data.results);
+
+        setArticles(response.data.results);
+      } catch (error) {
+        console.error("Fetching news failed:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchNews();
+  }, []);
 
   return (
-    <div className="">
-      <Header />
-      <Head>
-        <title>news aggregtot</title>
-      </Head>
-      <h1>latest news</h1>
-      <NewsList />
-      <h1>Hello Usman Khan</h1>
-      {/* <Component {...pageProps} /> */}
+    <div>
+      <h1>Latest News</h1>
+      {console.log(articles)};
+      {articles.map((article, index) => (
 
-      <Footer />
+        <div key={index}>
+          <h2>{article.title}</h2>
+          <p>{article.description}</p>
+          <a
+            href={article.url} target="_blank" rel="noopener noreferrer"
+          />
+        </div>
+      ))}
+      
+      {/* {loading ? (
+        <p>Loading news...</p>
+      ) : articles.length > 0 ? (
+        articles.map((article, index) => (
+          <div key={index}>
+            <h2>{article.title}</h2>
+            <p>{article.description}</p>
+            <a href={article.url} target="_blank" rel="noopener noreferrer">
+              Read More
+            </a>
+          </div>
+        ))
+      ) : (
+        <p>No news available.</p>
+      )} */}
     </div>
   );
 }
