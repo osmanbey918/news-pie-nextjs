2882c476b1257733439657d645521d99dba86fee
Usman Khan <usman918khan@gmail.com>
2025-03-24 21:05:19 +0500

    improved

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import Card from "./components/newscard/Card";
+import Link from "next/link";
 
 export default function Home() {
   const [articles, setArticles] = useState([]);
@@ -24,6 +25,14 @@ export default function Home() {
 
   return (
     <div>
+      <nav>
+        <ul>
+          <Link href="/factquiz" className="p-6 color-red">Quiz</Link>
+          <Link href="/funfact">FunFact</Link>
+          <Link href="/scrape">scrape</Link>
+          <Link href="/service">service</Link>
+        </ul>
+      </nav>
       <h1>Latest News</h1>
       {loading ? (
         <p>Loading...</p>
